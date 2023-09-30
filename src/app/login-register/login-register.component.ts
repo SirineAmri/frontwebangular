85dd74c6ea27c3c8a2bbab6c69cd254621ed024f
AzzaBenRomdhan <azza.benromdhan@esprit.tn>
2023-09-30 23:25:36 +0100

    consommationLoginRegister

--- src/app/login-register/login-register.component.ts
+++ src/app/login-register/login-register.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { AfterViewInit, Renderer2, ElementRef } from '@angular/core';
+import { User } from '../model/User';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
+import { UserAuthService } from '../services/user-auth.service';
+import { NgForm } from '@angular/forms';
 
 
 @Component({
@@ -8,7 +13,21 @@ import { AfterViewInit, Renderer2, ElementRef } from '@angular/core';
   styleUrls: ['./login-register.component.css']
 })
 export class LoginRegisterComponent  implements AfterViewInit {
-  constructor(private renderer: Renderer2, private el: ElementRef){}
+  newUser: User = {
+    userName: '',
+    userFirstName: '',
+    userLastName: '',
+    userPassword: '',
+    role : [],
+  };
+  registrationSuccess = false;
+  registrationError = false;
+
+  userRole = '';
+
+  constructor(private renderer: Renderer2, private el: ElementRef, 
+    private userService: UserService, private router: Router, private userAuthService: UserAuthService,
+    ){}
   
   ngAfterViewInit() {
     const signUpButton = this.el.nativeElement.querySelector('#signUp');
@@ -24,4 +43,49 @@ export class LoginRegisterComponent  implements AfterViewInit {
       });
     }
   }
+
+  registerUser() {
+    this.userService.registerNewUser(this.newUser).subscribe(
+      (response) => {
+        this.showSuccessAlert('Matricule valide Compte créé avec succès.');
+        this.router.navigate(['/login']);
+      },
+      (error) => {
+          this.showErrorAlert(' Erreur lors de la création du compte Matricule invalide. Veuillez saisir un matricule existant.');
+        }
+    );
+  }
+   // tslint:disable-next-line:typedef
+   showSuccessAlert(message: string) {
+    alert('Succès: ' + message);
+  }
+
+  // tslint:disable-next-line:typedef
+  showErrorAlert(error: string) {
+    alert('Erreur: ' + error);
+  }
+
+    // tslint:disable-next-line:typedef
+    login(loginForm: NgForm) {
+      this.userService.login(loginForm.value).subscribe(
+        (response: any) => {
+          this.userAuthService.setRoles(response.user.role);
+          this.userAuthService.setToken(response.jwtToken);
+  
+          const role = response.user.role[0].roleName;
+          if (role === 'undefined') {
+            this.router.navigate(['']);
+          }
+          if (role === 'Admin') {
+            this.router.navigate(['/admin']);
+          } else {
+            this.router.navigate(['/match']);
+          }
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+
 }
---
+++ src/app/model/User.ts
@@ -0,0 +1,7 @@
+export class User {
+    userName: any;
+    userFirstName: any;
+    userLastName: any;
+    userPassword: any;
+    role : any;
+}
