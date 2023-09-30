dab00ad76dbcac1d9eeba3bc521c0ffaac8b1b1f
AzzaBenRomdhan <azza.benromdhan@esprit.tn>
2023-09-30 23:57:29 +0100

    NvigationbtnMatch

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserAuthService } from '../services/user-auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  constructor(private userAuthService: UserAuthService, private router : Router){}
+  redirectToMatch(): void {
+    // Vérifiez si l'utilisateur est déjà connecté
+    if (this.userAuthService.isLoggedIn()) {
+      // L'utilisateur est connecté, effectuez la redirection vers /match
+      this.router.navigate(['/match']);
+    } else {
+      // L'utilisateur n'est pas connecté, redirigez-le vers la page de connexion/inscription
+      this.router.navigate(['/loginRegister']);
+    }
+  }
 }
